63815b7c6e5533811c76021a60675d3d13c79730
pierreolivierroy <roy.pierre.o@gmail.com>
2022-01-22 14:43:38 -0500

    [code-challenge] Create reservation service base project

---
+++ reservation-service/src/main/java/upgrade/challenge/reservation/ReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package upgrade.challenge.reservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationServiceApplication.class, args);
+    }
+
+}
