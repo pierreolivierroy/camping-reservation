0e7963c25c6203fc5ab2fb17130c631acdf3e28b
poroy <pierreolivier.roy@bell.ca>
2022-01-25 21:42:37 -0500

    [code-challenge] Build availability service base foundation

---
+++ availability-service/src/main/java/upgrade/challenge/availability/AvailabilityServiceApplication.java
@@ -0,0 +1,13 @@
+package upgrade.challenge.availability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvailabilityServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvailabilityServiceApplication.class, args);
+    }
+
+}
